79166c75a4eb122785df0c9aa5b8ba5f4ae39228
Renato Groffe <renato.groffe@yahoo.com.br>
2021-11-26 19:55:28 -0300

    .NET 6 + Class Library + Configurações NuGet + Conversão de Alturas
    .NET 6 + Class Library + Configurações NuGet + Conversão de Alturas

---
+++ Groffe.Alturas/ConversorAlturas.cs
@@ -0,0 +1,18 @@
+﻿namespace Groffe.Alturas;
+
+public class ConversorAlturas
+{
+    /// <summary>
+    /// Converte uma altura em pes para o equivalente em metros
+    /// </summary>
+    /// <param name="alturaPes">Altura em pes</param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentException">Excecao lancada para uma altura em pes negativa ou igual a zero.</exception>
+    public static double PesParaMetros(double alturaPes)
+    {
+        if (alturaPes <= 0)
+            throw new ArgumentException("A altura em pes deve ser um valor maior do que zero!");
+        
+        return Math.Round(alturaPes * 0.3048, 4);
+    }
+}
